18a18cf31a27a7ba6e660316e6f19872f864fd9c
Ayano206 <113108261+AAAAyano206@users.noreply.github.com>
2023-03-06 16:16:29 +0800

    Update and rename main.cpp to resize.cpp

--- resize/resize.cpp
+++ resize/resize.cpp
@@ -1,48 +1,53 @@
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
-#include <math.h>
 #include <iostream>
-#include <stdlib.h>
 
 using namespace cv;
 using namespace std;
 
+// 遍历文件夹内的所有图像
 int getImagePathList(std::string folder, std::vector<cv::String>& imagePathList)
 {
-	// search all the image in a folder
-	glob(folder, imagePathList);
+	cv::glob(folder, imagePathList);
 	return 0;
 }
 
+// 获取图像的文件名
 string getName(string str) {
-  // get image name from its path
-	int end = str.size() -1;
-	while(str[end] != '\\')
-      --end;
-	return str.substr(end + 1);
+	int length = str.size();
+	int start = 0;
+	for (int i = length - 1; i >= 0; --i) {
+		if (str[i] == '\\') {
+			start = i;
+			break;
+		}
+	}
+	return str.substr(start + 1);
 }
 
 int main()
 {
-	String folder = "D:\\dataset";
-	vector<String> imagePathList;
+	// 请修改为自己的文件夹路径
+	cv::String folder = "C:\\Users\\Lunci\\Desktop\\dataset\\high";
+	std::vector<cv::String> imagePathList;
 	getImagePathList(folder, imagePathList);
 	for (int i = 0; i < imagePathList.size(); i++)
 	{
-		cout << imagePathList[i] << "\n";
+		std::cout << imagePathList[i] << "\n";
 		auto srcImage = cv::imread(imagePathList[i]);
 		if (srcImage.empty())
 		{
-			cout << "cannot read the image, retry plz！\n";
+			cout << "读取图像有误，请重新输入正确路径！\n";
 			return -1;
 		}
-    // start to resize
+
+		// 按照指定的宽、高对图像进行尺寸变换
 		Size dsize = Size(600, 400);
 		Mat output;
 		resize(srcImage, output, dsize, 0, 0, INTER_AREA);
 		string name = getName(imagePathList[i]);
-		string outPath = "D:\\new_dataset" + '\\' + name;
+		string outPath = "C:\\Users\\Lunci\\Desktop\\dataset\\new_high\\" + name;
 		imwrite(outPath, output);
 
 		waitKey(100);
